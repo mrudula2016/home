8fc97e061182320960c358d6db110e066c9b36e4
terrywilliam <terrywilliam@Blue>
2020-03-10 14:52:34 +0000

    class has been created for UI service and doa

---
+++ timesheet-app/src/main/java/office/timesheet/ui/TimesheetDto.java
@@ -0,0 +1,52 @@
+package office.timesheet.ui;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@javax.persistence.Entity
+@Table(name = "timesheet")
+public class TimesheetDto {
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String emailId;
+	private String password;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+}
---
+++ timesheet-app/src/main/java/office/timesheet/ui/TimesheetUi.java
@@ -0,0 +1,43 @@
+package office.timesheet.ui;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+
+import office.timesheet.service.TimesheetService;
+@ManagedBean(name="clockify")
+public class TimesheetUi {
+	@ManagedProperty(value = "#{tss}")
+	private TimesheetService timesheetservice;
+	private TimesheetDto timesheetDto;
+	private List<TimesheetDto> dto;
+
+	public List<TimesheetDto> getDto() {
+		return dto;
+	}
+
+	public void setDto(List<TimesheetDto> dto) {
+		this.dto = dto;
+	}
+
+	public TimesheetDto getTimesheetDto() {
+		return timesheetDto;
+	}
+
+	public void setTimesheetDto(TimesheetDto timesheetDto) {
+		this.timesheetDto = timesheetDto;
+	}
+
+	public TimesheetService getTimesheetservice() {
+		return timesheetservice;
+	}
+
+	public void setTimesheetservice(TimesheetService timesheetservice) {
+		this.timesheetservice = timesheetservice;
+	}
+	
+	public void addDataUi() {
+		timesheetservice.addDataService(timesheetDto);
+	}
+}
